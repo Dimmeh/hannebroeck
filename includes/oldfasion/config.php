20ae11762383fcab737e8edea9b84a4d3551e6dc
Dimmy Marcelissen <dimwes@hotmail.com>
2016-01-13 23:04:37 +0100

    config online

--- includes/oldfasion/config.php
+++ includes/oldfasion/config.php
@@ -6,15 +6,15 @@
  * Time: 15:03
  */
 
-$host = 'localhost';
-$username = '0904540';
-$password = 'thaciesh';
-$db = '0904540';
-//
 //$host = 'localhost';
-//$username = 'root';
-//$password = '';
-//$db = 'hann_reservation';
+//$username = '0904540';
+//$password = 'thaciesh';
+//$db = '0904540';
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'hann_reservation';
 
 $conn = new mysqli($host, $username, $password, $db);
 
